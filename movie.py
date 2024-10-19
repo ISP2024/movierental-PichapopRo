cd1649c4af6abd68a61802de3237f6997483c8c3
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-10-19 20:26:09 +0700

    Fix Movie Lazy class and add year and genre attribute. Make Movie immutable.

--- movie.py
+++ movie.py
@@ -1,19 +1,22 @@
+from collections.abc import Collection
+
 from price_strategies import PriceStrategy
+from dataclasses import dataclass
 
 
+@dataclass(frozen=True)
 class Movie:
     """
     A movie available for rent.
     """
+    title: str
+    year: int
+    genre: Collection[str]
 
-    # The types of movies (price_code).
+    def is_genre(self, genre: str) -> bool:
+        """Checks if the movie belongs to the given genre."""
+        return genre.lower() in self.genre
 
-    def __init__(self, title):
-        # Initialize a new movie.
-        self.title = title
-
-    def get_title(self):
-        return self.title
-
-    def __str__(self):
-        return self.title
+    def __str__(self) -> str:
+        """Returns the movie title and year in a formatted string."""
+        return f"{self.title} ({self.year})"
