65dcbe852313f55aa54e246701d4a206d2a65c7c
Pichapop Roddauyboon <pichapop.ro@ku.th>
2024-10-20 15:33:11 +0700

    Corrected rental_test.py

--- rental_test.py
+++ rental_test.py
@@ -8,23 +8,17 @@ class RentalTest(unittest.TestCase):
 
     def setUp(self):
 
-        self.new_movie = Movie("Dune: Part Two", NewReleasePrice())
-        self.regular_movie = Movie("Air", RegularPrice())
-        self.childrens_movie = Movie("Frozen", ChildrensPrice())
-
-    def test_movie_attributes(self):
-        """Trivial test to catch refactoring errors or changes in the API of Movie."""
-        m = Movie("Air", RegularPrice())
-        self.assertEqual("Air", m.get_title())
-        self.assertIsInstance(m.price_code, RegularPrice)
+        self.new_movie = Movie("Dune: Part Two", 2023, {"Action"})
+        self.regular_movie = Movie("Air", 2023, {"Drama"})
+        self.childrens_movie = Movie("Frozen", 2013, {"Animation", "Children"})
 
     def test_rental_price(self):
         """Test get_price method in Rental after refactoring."""
         # Test New Release movie
         rental = Rental(self.new_movie, 1)
-        self.assertEqual(rental.get_price(), 3.0)
+        self.assertEqual(rental.get_price(), 2.0)
         rental = Rental(self.new_movie, 5)
-        self.assertEqual(rental.get_price(), 15.0)
+        self.assertEqual(rental.get_price(), 6.5)
 
         # Test for Regular movie
         rental = Rental(self.regular_movie, 1)
@@ -42,7 +36,7 @@ class RentalTest(unittest.TestCase):
         rental = Rental(self.new_movie, 1)
         self.assertEqual(rental.get_rental_points(), 1)
         rental = Rental(self.new_movie, 3)
-        self.assertEqual(rental.get_rental_points(), 3)
+        self.assertEqual(rental.get_rental_points(), 1)
         rental = Rental(self.regular_movie, 3)
         self.assertEqual(rental.get_rental_points(), 1)
         rental = Rental(self.childrens_movie, 3)
